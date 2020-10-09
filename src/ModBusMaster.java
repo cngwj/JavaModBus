52c2f91c27b1c33ef10b51d6e19828d298110fa3
gaoshan <cngwj@outlook.com>
2020-10-09 22:36:04 +0800

    Modbus 功能码03

--- src/ModBusMaster.java
+++ src/ModBusMaster.java
@@ -1,6 +1,4 @@
 import java.net.InetAddress;
-import java.util.Date;
-
 import com.intelligt.modbus.jlibmodbus.Modbus;
 import com.intelligt.modbus.jlibmodbus.exception.ModbusIOException;
 import com.intelligt.modbus.jlibmodbus.exception.ModbusNumberException;
@@ -19,7 +17,7 @@ public class ModBusMaster {
             TcpParameters tcpParameters = new TcpParameters();
 
             // 设置TCP的ip地址
-            InetAddress adress = InetAddress.getByName("192.168.0.177");
+            InetAddress adress = InetAddress.getByName("192.168.0.10");
 
             // TCP参数设置ip地址
             // tcpParameters.setHost(InetAddress.getLocalHost());
@@ -36,7 +34,7 @@ public class ModBusMaster {
 
             int slaveId = 1;//从机地址
             int offset = 0;//寄存器读取开始地址
-            int quantity = 10;//读取的寄存器数量
+            int quantity = 20;//读取的寄存器数量
 
 
             try {
@@ -45,9 +43,10 @@ public class ModBusMaster {
                     m.connect();// 开启连接
                 }
 
-                // 读取对应从机的数据，readInputRegisters读取的写寄存器，功能码04
-                int[] registerValues = m.readInputRegisters(slaveId, offset, quantity);
-
+//                // 读取对应从机的数据，readInputRegisters读取的写寄存器，功能码04
+//                int[] registerValues = m.readInputRegisters(slaveId, offset, quantity);
+                // 读取对应从机的数据，readHoldingRegisters读取的写寄存器，功能码03
+                int[] registerValues = m.readHoldingRegisters(slaveId, offset, quantity);
                 // 控制台输出
 
                 for (int value : registerValues) {
